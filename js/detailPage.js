e04f49625a1869880649298d0dd9cc0fb9d35bd2
ElishreeDey <elishreed@mindfiresolutions.com>
2026-04-21 20:04:28 +0530

    Modified functions for editing data, deleting data from table and from local storage, adding data to table and local JSON array

--- js/detailPage.js
+++ js/detailPage.js
@@ -57,75 +57,97 @@ function saveData(){
         alert("Data saved successfully!");
     }
 
-    //save to local storage
-    localStorage.setItem("userName", name);
-    localStorage.setItem("email", email);
-    localStorage.setItem("phone", phone);
-    localStorage.setItem("gender", gender);   
-
-    let table = document.getElementById("viewData"); 
-    // Insert a new row at the end (-1 adds to the last position)
-    let newRow = table.insertRow(-1); 
-    var setRowNumber = table.rows.length - 1;
-
-    // Set the dynamic ID to created table row.
-    newRow.id = "row_" + setRowNumber; 
-
-    // Insert new cells into that created row and set dynamic IDs to each cell.
-    let cellOne = newRow.insertCell(0); cellOne.id = "cellOne_" + setRowNumber; 
-    let cellTwo = newRow.insertCell(1); cellTwo.id = "cellTwo_" + setRowNumber; 
-    let cellThree = newRow.insertCell(2); cellThree.id = "cellThree_" + setRowNumber; 
-    let cellFour = newRow.insertCell(3); cellFour.id = "cellFour_" + setRowNumber; 
-    let cellFive = newRow.insertCell(4); cellFive.id = "cellFive_" + setRowNumber; 
-    let cellSix = newRow.insertCell(5); cellSix.id = "cellSix_" + setRowNumber; 
-
-    // Add content to the new cells
-    cellOne.innerHTML = localStorage.getItem("userName");
-    cellTwo.innerHTML = localStorage.getItem("email");
-    cellThree.innerHTML = localStorage.getItem("phone");
-    cellFour.innerHTML = localStorage.getItem("gender");
-    cellFive.innerHTML = "<span value='Edit' onclick='editRow(this)'>Edit</span>";
-    cellSix.innerHTML = "<span value='X' onclick='deleteRow(this)'>X</span>";
-
-    // Setting colors to edit and delete(X) buttons
-    cellFive.style.color = "blue";
-    cellSix.style.color = "red";
+    //Create table from entered data and store to local storage
+    createTableFromData(name, email, phone,gender);
 
     //alert(document.getElementById("viewData").innerHTML);
     // clear entry fields after submission
-    document.getElementById("userName").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("phone").value = "";
-    document.getElementById("gender").value = "";
-    document.getElementById("mandatoryName").style.display= 'block';
-    document.getElementById("mandatoryEmail").style.display= 'block';
-    document.getElementById("mandatoryPhone").style.display= 'block';
+    clearEntryFields();
+
+
+
+
+    //-----------------------------------------------------------
+    // Storing data:
+    const setLocalStorageJSON = JSON.parse(localStorage.getItem("setLocalStorageJSON")) || [];
+    let entryDataObj = { name: name, email: email, phone: phone, gender: gender };
+    let entryDataJSON = JSON.stringify(entryDataObj); 
+    setLocalStorageJSON.push(entryDataJSON);
+    localStorage.setItem("setLocalStorageJSON", JSON.stringify(setLocalStorageJSON));
+    const allEnteredvalues = JSON.parse(localStorage.getItem("setLocalStorageJSON")) || []; // Parse string to array
+    
+    // Insert a new row at the end (-1 adds to the last position)
+    //let newRow = table.insertRow(-1); alert(newRow);
+    //const firstJSONNode = allEnteredvalues[0]; // Gets the 1st node of json object
+
+    //alert(firstJSONNode); // Access the 1st node of json object
+    //-----------------------------------------------------------
+
+
+
+
 
 }
 
 //This function will delete the corresponding row of the table when user clicks on delete(X) button.
 function deleteRow(r) {
-  var i = r.parentNode.parentNode.rowIndex;
-  document.getElementById("viewData").deleteRow(i);
+    if (confirm("Are you sure you want to delete this rec?")) {
+        var i = r.parentNode.parentNode.rowIndex;
+        document.getElementById("viewData").deleteRow(i);
+
+        //It will remove the data from localstorage JSON array as well. So on page it will not visible as data is deleted from table.  
+        let data = JSON.parse(localStorage.getItem("setLocalStorageJSON")) || []; // Parse string to array
+        const index = 1; // position to delete
+        if (index > -1 && index < data.length) {
+          data.splice(index, 1); // remove 1 item at index
+        }
+        localStorage.setItem("setLocalStorageJSON", JSON.stringify(data));
+        // End of code for deleting data from local storage JSON array as well. 
+
+    } else {
+        alert("Deletion cancelled.");
+    }
 }
 
-/*function editRow(r) {//alert("edit");
+function editRow(r) {//alert("edit");
   var i = r.parentNode.parentNode.rowIndex; 
   const rowId = r.parentNode.parentNode.id;//alert(rowId);
-  document.getElementById("editTableRowNo").innerHTML= rowId;
-  //alert(i);
-  var nameForEdit = document.getElementById("viewData").rows[i].cells[0].innerHTML;
-  var emailForEdit = document.getElementById("viewData").rows[i].cells[1].innerHTML;
-  var phoneForEdit = document.getElementById("viewData").rows[i].cells[2].innerHTML;
-  var genderForEdit = document.getElementById("viewData").rows[i].cells[3].innerHTML;
+    document.getElementById("editTableRowNo").innerHTML= rowId;
+    //alert(i);
+    var nameForEdit = document.getElementById("viewData").rows[i].cells[0].innerHTML;
+    var emailForEdit = document.getElementById("viewData").rows[i].cells[1].innerHTML;
+    var phoneForEdit = document.getElementById("viewData").rows[i].cells[2].innerHTML;
+    var genderForEdit = document.getElementById("viewData").rows[i].cells[3].innerHTML;
 
-  //alert(nameForEdit + " " + emailForEdit + " " + phoneForEdit + " " + genderForEdit);
-  document.getElementById("userName").value = nameForEdit;
-  document.getElementById("email").value = emailForEdit;
-  document.getElementById("phone").value = phoneForEdit;
-  document.getElementById("gender").value = genderForEdit;
-  
-}*/
+    //alert(nameForEdit + " " + emailForEdit + " " + phoneForEdit + " " + genderForEdit);
+    document.getElementById("userName").value = nameForEdit;
+    document.getElementById("email").value = emailForEdit;
+    document.getElementById("phone").value = phoneForEdit;
+    document.getElementById("gender").value = genderForEdit;
+    
+    document.getElementById("btnAddData").style.display= 'none';
+    document.getElementById("btnEditData").style.display= 'block';
+}
+
+function saveEditedData(){//alert("save edit");
+    var rowId = document.getElementById("editTableRowNo").innerHTML; //alert(rowId);
+    var i = Number(rowId.split("_")[1]); //alert(i);
+    document.getElementById("cellOne_" + i).innerHTML= document.getElementById("userName").value;
+    document.getElementById("cellTwo_" + i).innerHTML= document.getElementById("email").value;
+    document.getElementById("cellThree_" + i).innerHTML= document.getElementById("phone").value;
+    document.getElementById("cellFour_" + i).innerHTML= document.getElementById("gender").value;
+    document.getElementById("btnAddData").style.display= 'block';
+    document.getElementById("btnEditData").style.display= 'none';
+
+    //Trying to update edited data to local storage as well. Below code is not working as expected, need to check and fix it.
+    //let setLocalStorageJSON = JSON.parse(localStorage.getItem("setLocalStorageJSON")) || {};
+    //setLocalStorageJSON.name = document.getElementById("userName").value; alert(setLocalStorageJSON.name);
+    //localStorage.setItem("setLocalStorageJSON", JSON.stringify(setLocalStorageJSON));
+    //End of code for updating edited data to local storage. Need to check and fix it.
+
+    // clear entry fields after submission
+    clearEntryFields();
+}
 
 //This function will check if the name is empty or not. Can be used for other fields as well if needed to check NotIsEmpty.
 function checkNotIsEmpty(name) {  
@@ -169,4 +191,70 @@ function validateFlexiblePhone(phone) {
     const formatted = phone.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3'); // basic phone format as xxx-xxx-xxxx
     document.getElementById("phone").value = formatted;
   }
+}
+
+function clearEntryFields(){
+    // clear entry fields after submission
+    document.getElementById("userName").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("gender").value = "";
+    document.getElementById("mandatoryName").style.display= 'block';
+    document.getElementById("mandatoryEmail").style.display= 'block';
+    document.getElementById("mandatoryPhone").style.display= 'block';
+}
+
+function displayData(){
+  //alert("on load display data");
+  const allEnteredvalues = JSON.parse(localStorage.getItem("setLocalStorageJSON")) || []; // Parse string to array
+  //alert(allEnteredvalues); 
+
+  const totalCount = allEnteredvalues.length;
+  //alert(totalCount);
+
+  for (let loopCount = 0; loopCount < totalCount; loopCount++) {
+    const firstJSONNode = allEnteredvalues[loopCount]; // Gets one node of json object based on loop count
+    //alert(firstJSONNode); 
+    const obj = JSON.parse(firstJSONNode);//parse json string to json object to access individual values
+    createTableFromData(obj.name, obj.email, obj.phone, obj.gender);
+  }     
+}
+
+function createTableFromData(name, email, phone,gender){
+//alert("create table from data");
+  //save to local storage
+    localStorage.setItem("userName", name);
+    localStorage.setItem("email", email);
+    localStorage.setItem("phone", phone);
+    localStorage.setItem("gender", gender);  
+
+
+    let table = document.getElementById("viewData"); 
+    // Insert a new row at the end (-1 adds to the last position)
+    let newRow = table.insertRow(-1); 
+    var setRowNumber = table.rows.length - 1;
+
+    // Set the dynamic ID to created table row.
+    newRow.id = "row_" + setRowNumber; 
+
+    // Insert new cells into that created row and set dynamic IDs to each cell.
+    let cellOne = newRow.insertCell(0); cellOne.id = "cellOne_" + setRowNumber; 
+    let cellTwo = newRow.insertCell(1); cellTwo.id = "cellTwo_" + setRowNumber; 
+    let cellThree = newRow.insertCell(2); cellThree.id = "cellThree_" + setRowNumber; 
+    let cellFour = newRow.insertCell(3); cellFour.id = "cellFour_" + setRowNumber; 
+    let cellFive = newRow.insertCell(4); cellFive.id = "cellFive_" + setRowNumber; 
+    let cellSix = newRow.insertCell(5); cellSix.id = "cellSix_" + setRowNumber; 
+
+    // Add content to the new cells
+    cellOne.innerHTML = localStorage.getItem("userName");
+    cellTwo.innerHTML = localStorage.getItem("email");
+    cellThree.innerHTML = localStorage.getItem("phone");
+    cellFour.innerHTML = localStorage.getItem("gender");
+    cellFive.innerHTML = "<span value='Edit' onclick='editRow(this)'>Edit</span>";
+    cellSix.innerHTML = "<span value='X' onclick='deleteRow(this)'>X</span>";
+
+    // Setting colors to edit and delete(X) buttons
+    cellFive.style.color = "blue";
+    cellSix.style.color = "red";
+
 }
